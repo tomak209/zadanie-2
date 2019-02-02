35d71feaf07bda5543f9fa0e4662ecc85ab8c802
Tomasz <44137002+tomak209@users.noreply.github.com>
2019-02-02 15:48:33 +0100

    Create kod - wersja 2.py

---
+++ kod - wersja 2.py
@@ -0,0 +1,35 @@
+import unittest
+from selenium import webdriver
+
+class fail_register_new_accountt(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def shutDown(self):
+        self.browser.close()
+    
+    def test_registration_wizzair_account(self):
+        cos = self.cos
+        cos.get("https://wizzair.com/pl-pl#/")
+        cos.find_elements_by_class_name('navigation__button navigation__button--simple').click()
+        cos.find_elements_by_class_name('content__link1').click()
+        cos.find_elemebts_by_name('firstName').click()
+        cos.find_elemebts_by_name('firstName').clear()
+        cos.find_elemebts_by_name('firstName').send_keys("Jonasz")
+        cos.find_elemebts_by_name('lastName').click()
+        cos.find_elemebts_by_name('lastName').clear()
+        cos.find_elemebts_by_name('lastName').send_keys("Zsanoj")
+        cos.find_elements_by_class_name('rf-switch__label').click()
+        cos.find_elemebts_by_name('mobilePhone').click()
+        cos.find_elemebts_by_name('mobilePhone').clear()
+        cos.find_elemebts_by_name('mobilePhone').send_keys('71661234567')
+        cos.find_elemebts_by_name('email').click()
+        cos.find_elemebts_by_name('email').clear()
+        cos.find_elemebts_by_name('email').send_keys('Jonasz.Zsanoj@gmail.cooooom')
+        cos.find_elemebts_by_name('password').click()
+        cos.find_elemebts_by_name('password').clear()
+        cos.find_elemebts_by_name('password').send_keys('zaq1@WSX')
+        cos.find_elements_by_class_name('rf-input__input rf-input__input--empty').click()
+        assert secondary_variable == "Invalid e-mail"
+        
+
